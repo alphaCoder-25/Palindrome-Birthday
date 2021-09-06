756879e2c9d87b2f403e2bcba558a64c731c58be
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-06 18:19:30 +0530

    some changes

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const str = 'hello';
-const charList = str.split('');
-const reversedList = charList.reverse();
-const reversedString = reversedList.join('');
-console.log(reversedString)
+
+function reverseString(str) { 
+    const charList = str.split('');
+    const reversedList = charList.reverse();
+    const reversedString = reversedList.join('');
+    return reversedString;
+};
