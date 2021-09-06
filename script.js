0540b3c5a2659823188c38cc623bc06f254e0cfa
anurudra555 <72567564+anurudra555@users.noreply.github.com>
2021-09-06 19:34:36 +0530

    coverting date to string

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function isPalindrome(str) {
     const reverse = reverseString(str);
     return str === reverse;
 };
+
+const date = {
+    day: 25,
+    month: 05,
+    year: 1997 
+};
+
+function convertDateToString(date) {
+
+    const dateString = { day: '', month: '', year: ''};
+    
+    if( date.day < 10 ) {
+        dateString.day = '0' + date.day;
+    } else {
+        dateString.day = date.day.toString();
+    }
+
+    if( date.month < 10 ) {
+        dateString.month = '0' + date.month;
+     } else {
+        dateString.month = date.month.toString();
+     }
+
+      dateString.year = date.year.toString();
+
+     return dateString;
+};
+console.log(convertDateToString(date));
